91a16f5c0ce003bde22e46b0df40235005d8c566
William1Gray <wgray5093@gmail.com>
2022-03-22 15:50:40 -0300

    Change burger to just a logout button

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -9,7 +9,7 @@ import {
   Menu,
   Divider,
   Tabs,
-  Burger,
+  Button,
   Image,
 } from '@mantine/core';
 import { useBooleanToggle } from '@mantine/hooks';
@@ -115,57 +115,9 @@ export default function Header({ user, tabs }: HeaderTabs) {
                 height={50}
             />
 
-          <Burger
-            opened={opened}
-            onClick={() => toggleOpened()}
-            className={classes.burger}
-            size="sm"
-          />
-
-          <Menu
-            size={260}
-            placement="end"
-            transition="pop-top-right"
-            className={classes.userMenu}
-            onClose={() => setUserMenuOpened(false)}
-            onOpen={() => setUserMenuOpened(true)}
-            control={
-              <UnstyledButton
-                className={cx(classes.user, { [classes.userActive]: userMenuOpened })}
-              >
-                <Group spacing={7}>
-                  <Avatar src={user.image} alt={user.name} radius="xl" size={20} />
-                  <Text weight={500} size="sm" sx={{ lineHeight: 1 }} mr={3}>
-                    {user.name}
-                  </Text>
-                  <ChevronDown size={12} />
-                </Group>
-              </UnstyledButton>
-            }
-          >
-            <Menu.Item icon={<Heart size={14} color={theme.colors.red[6]} />}>
-              Liked posts
-            </Menu.Item>
-            <Menu.Item icon={<Star size={14} color={theme.colors.yellow[6]} />}>
-              Saved posts
-            </Menu.Item>
-            <Menu.Item icon={<Message size={14} color={theme.colors.blue[6]} />}>
-              Your comments
-            </Menu.Item>
-
-            <Menu.Label>Settings</Menu.Label>
-            <Menu.Item icon={<Settings size={14} />}>Account settings</Menu.Item>
-            <Menu.Item icon={<SwitchHorizontal size={14} />}>Change account</Menu.Item>
-            <Menu.Item icon={<Logout size={14} />}>Logout</Menu.Item>
-
-            <Divider />
-
-            <Menu.Label>Danger zone</Menu.Label>
-            <Menu.Item icon={<PlayerPause size={14} />}>Pause subscription</Menu.Item>
-            <Menu.Item color="red" icon={<Trash size={14} />}>
-              Delete account
-            </Menu.Item>
-          </Menu>
+            <Button radius="md" variant="subtle" leftIcon={<Logout size={14} />}>
+                Logout
+            </Button>
         </Group>
       </Container>
       <Container>
