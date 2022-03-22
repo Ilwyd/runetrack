2b1c1205437998c477eaf67a17b86b8f43b01609
William1Gray <wgray5093@gmail.com>
2022-03-22 16:19:11 -0300

    Put footer into a container

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,4 +1,4 @@
-import { ActionIcon, useMantineColorScheme } from '@mantine/core';
+import { ActionIcon, Container, createStyles, useMantineColorScheme } from '@mantine/core';
 import { Sun, MoonStars } from 'tabler-icons-react';
 
 function Footer() {
@@ -6,14 +6,16 @@ function Footer() {
     const dark = colorScheme === 'dark';
     
     return (
-        <ActionIcon
-            variant="outline"
-            color={dark ? 'yellow' : 'blue'}
-            onClick={() => toggleColorScheme()}
-            title="Toggle color scheme"
-        >
-            {dark ? <Sun size={18} /> : <MoonStars size={18} />}
-        </ActionIcon>
+        <Container>
+            <ActionIcon
+                variant="outline"
+                color={dark ? 'yellow' : 'blue'}
+                onClick={() => toggleColorScheme()}
+                title="Toggle color scheme"
+            >
+                {dark ? <Sun size={18} /> : <MoonStars size={18} />}
+            </ActionIcon>
+        </Container>
     );
 }
 
