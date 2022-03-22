5de14e640fa1ef1bcbd02f53f0a224685e6a1a07
William1Gray <wgray5093@gmail.com>
2022-03-22 16:04:18 -0300

    Remove unused imports and other code

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,28 +1,15 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import {
   createStyles,
   Container,
-  Avatar,
-  UnstyledButton,
   Group,
-  Text,
-  Menu,
-  Divider,
   Tabs,
   Button,
   Image,
 } from '@mantine/core';
 import { useBooleanToggle } from '@mantine/hooks';
 import {
-  Logout,
-  Heart,
-  Star,
-  Message,
-  Settings,
-  PlayerPause,
-  Trash,
-  SwitchHorizontal,
-  ChevronDown,
+  Logout
 } from 'tabler-icons-react/';
 
 const useStyles = createStyles((theme) => ({
@@ -37,33 +24,6 @@ const useStyles = createStyles((theme) => ({
 
   mainSection: {
     paddingBottom: theme.spacing.sm,
-  },
-
-  userMenu: {
-    [theme.fn.smallerThan('xs')]: {
-      display: 'none',
-    },
-  },
-
-  user: {
-    color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black,
-    padding: `${theme.spacing.xs}px ${theme.spacing.sm}px`,
-    borderRadius: theme.radius.sm,
-    transition: 'background-color 100ms ease',
-
-    '&:hover': {
-      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.white,
-    },
-  },
-
-  burger: {
-    [theme.fn.largerThan('xs')]: {
-      display: 'none',
-    },
-  },
-
-  userActive: {
-    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.white,
   },
 
   tabs: {
@@ -93,14 +53,11 @@ const useStyles = createStyles((theme) => ({
 }));
 
 interface HeaderTabs {
-  user: { name: string; image: string };
   tabs: string[];
 }
 
-export default function Header({ user, tabs }: HeaderTabs) {
+export default function Header({ tabs }: HeaderTabs) {
   const { classes, theme, cx } = useStyles();
-  const [opened, toggleOpened] = useBooleanToggle(false);
-  const [userMenuOpened, setUserMenuOpened] = useState(false);
 
   const items = tabs.map((tab) => <Tabs.Tab label={tab} key={tab} />);
 
