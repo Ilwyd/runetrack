5890a80180e021e6a4117afe39e9f6711f6b5080
William1Gray <wgray5093@gmail.com>
2022-03-22 18:12:42 -0300

    Update footer style

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,21 +1,35 @@
 import { ActionIcon, Container, createStyles, useMantineColorScheme } from '@mantine/core';
 import { Sun, MoonStars } from 'tabler-icons-react';
 
+const useStyles = createStyles((theme) => ({
+    footer: {
+            paddingTop: theme.spacing.sm,
+            backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+            borderTop: `1px solid ${
+                theme.colorScheme === 'dark' ? 'transparent' : theme.colors.gray[2]
+            }`,
+            marginTop: 120,
+      }
+}))
+
 function Footer() {
     const { colorScheme, toggleColorScheme } = useMantineColorScheme();
     const dark = colorScheme === 'dark';
-    
+    const { classes, theme, cx } = useStyles();
+
     return (
-        <Container>
-            <ActionIcon
-                variant="outline"
-                color={dark ? 'yellow' : 'blue'}
-                onClick={() => toggleColorScheme()}
-                title="Toggle color scheme"
-            >
-                {dark ? <Sun size={18} /> : <MoonStars size={18} />}
-            </ActionIcon>
-        </Container>
+        <div className={classes.footer}>
+            <Container>
+                <ActionIcon
+                    variant="outline"
+                    color={dark ? 'yellow' : 'blue'}
+                    onClick={() => toggleColorScheme()}
+                    title="Toggle color scheme"
+                >
+                    {dark ? <Sun size={18} /> : <MoonStars size={18} />}
+                </ActionIcon>
+            </Container>
+        </div>
     );
 }
 
