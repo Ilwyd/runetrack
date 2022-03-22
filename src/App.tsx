d2765ab0d7b1418ae63b68d1d2034aee80ebc853
William1Gray <wgray5093@gmail.com>
2022-03-22 16:04:36 -0300

    Add dark mode switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header/Header'
 import Footer from './Components/Footer/Footer'
-import { MantineProvider } from '@mantine/core';
+import { MantineProvider, ColorSchemeProvider, ColorScheme } from '@mantine/core';
 
 const tabs = ["Dailies", "Weeklies", "Monthlies", "Favourites"]
 
 function App() {
+  const [colorScheme, setColorScheme] = useState<ColorScheme>('light');
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+
   return (
     <>
-      <MantineProvider theme={{colorScheme: 'dark'}}>
-        <Header tabs={tabs} user={{name: "test", image: ""}}/>
+      <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+      <MantineProvider theme={{colorScheme}}>
+        <Header tabs={tabs} />
+        <Footer />
       </MantineProvider>
+      </ColorSchemeProvider>
     </>
   );
 }
