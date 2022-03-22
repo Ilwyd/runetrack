74568499dc43d5196d2872c228977e193e20105e
William1Gray <wgray5093@gmail.com>
2022-03-22 15:52:49 -0300

    Change theme to dark

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header/Header'
 import Footer from './Components/Footer/Footer'
+import { MantineProvider } from '@mantine/core';
 
 const tabs = ["Dailies", "Weeklies", "Monthlies", "Favourites"]
 
 function App() {
   return (
     <>
-      <Header tabs={tabs} user={{name: "test", image: ""}}/>
+      <MantineProvider theme={{colorScheme: 'dark'}}>
+        <Header tabs={tabs} user={{name: "test", image: ""}}/>
+      </MantineProvider>
     </>
   );
 }
