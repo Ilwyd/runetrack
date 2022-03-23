654e9feed6fc82bd0a2d6e82d418ef801aa0a233
William1Gray <wgray5093@gmail.com>
2022-03-22 23:49:53 -0300

    Implement email password login

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -1,9 +1,128 @@
-function Login() {
-    return(
-        <>
-            Hello World
-        </>
-    )
-}
+import React from 'react';
+import { useForm, useToggle, upperFirst } from '@mantine/hooks';
+import {
+    TextInput,
+    PasswordInput,
+    Text,
+    Paper,
+    Group,
+    PaperProps,
+    Button,
+    Divider,
+    Checkbox,
+    Anchor,
+    Container,
+} from '@mantine/core';
+import { GoogleIcon } from './GoogleIcon';
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 
-export default Login;
+export default function Login(props: PaperProps<'div'>) {
+    const [type, toggle] = useToggle('login', ['login', 'register']);
+    const form = useForm({
+        initialValues: {
+            email: '',
+            name: '',
+            password: '',
+            terms: true,
+        },
+
+        validationRules: {
+            email: (val) => /^\S+@\S+$/.test(val),
+            password: (val) => val.length >= 6,
+        },
+    });
+    const auth = getAuth();
+
+    const createAccount = () => {
+        createUserWithEmailAndPassword(auth, form.values.email, form.values.password)
+            .then((userCredential) => {
+                return true;
+            })
+            .catch(async (error) => {
+
+            })
+    }
+
+    const login = () => {
+        signInWithEmailAndPassword(auth, form.values.email, form.values.password)
+            .then((userCredential) => {
+                return true
+            })
+    }
+
+    return (
+        <Container style={{
+            top: '50%',
+            transform: 'translate(0%, 50%)'
+        }}>
+            <Paper radius="md" p="xl" withBorder {...props}>
+                <Text size="lg" weight={500}>
+                    Welcome to RuneTrack, {type} with
+                </Text>
+
+                <Group grow mb="md" mt="md">
+                    <Button leftIcon={<GoogleIcon />} radius="xl">Google</Button>
+                </Group>
+
+                <Divider label="Or continue with email" labelPosition="center" my="lg" />
+
+                <form onSubmit={form.onSubmit(() => { 
+                    if (type === "register") {
+                        createAccount();
+                    }
+                    else {
+                        login();
+                    }
+
+                    })}
+                >
+                    <Group direction="column" grow>
+                        {type === 'register' && (
+                            <TextInput
+                                label="Name"
+                                placeholder="Your name"
+                                value={form.values.name}
+                                onChange={(event) => form.setFieldValue('name', event.currentTarget.value)}
+                            />
+                        )}
+
+                        <TextInput
+                            required
+                            label="Email"
+                            placeholder="Your email"
+                            value={form.values.email}
+                            onChange={(event) => form.setFieldValue('email', event.currentTarget.value)}
+                            error={form.errors.email && 'Invalid email'}
+                        />
+
+                        <PasswordInput
+                            required
+                            label="Password"
+                            placeholder="Your password"
+                            value={form.values.password}
+                            onChange={(event) => form.setFieldValue('password', event.currentTarget.value)}
+                            error={form.errors.password && 'Password should include at least 6 characters'}
+                        />
+
+                        {type === 'register' && (
+                            <Checkbox
+                                label="I accept terms and conditions"
+                                checked={form.values.terms}
+                                onChange={(event) => form.setFieldValue('terms', event.currentTarget.checked)}
+                            />
+                        )}
+                    </Group>
+
+                    <Group position="apart" mt="xl">
+                        <Anchor component="button" type="button" color="gray" onClick={() => toggle()} size="xs">
+                            {type === 'register'
+                                ? 'Already have an account? Login'
+                                : "Don't have an account? Register"}
+                        </Anchor>
+                        <Button type="submit">{upperFirst(type)}</Button>
+                    </Group>
+                </form>
+            </Paper>
+        </Container>
+    );
+}
