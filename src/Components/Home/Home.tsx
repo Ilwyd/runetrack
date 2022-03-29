18b4916a827d074e87300130990435384a6109be
William1Gray <wgray5093@gmail.com>
2022-03-28 21:17:14 -0300

    Add cardarea

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,12 +1,21 @@
+import { useState } from "react";
+import CardArea from "../CardArea/CardArea";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 
 const tabs = ["Dailies", "Weeklies", "Monthlies", "Favourites"]
 
 function Home()  {
+    const [activeTab, setActiveTab] = useState("Dailies")
+    
+    const tabChange = (active: number, tabKey: string) => {
+        setActiveTab(tabKey);
+    }
+
     return(
         <>
-            <Header tabs={tabs} />
+            <Header tabs={tabs} tabChange={tabChange}/>
+            <CardArea activeTab={activeTab} />
             <Footer />
         </>
     )
