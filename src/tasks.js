afebfdda17256c221c35e891fcc04785253c9d35
William1Gray <wgray5093@gmail.com>
2022-03-28 23:10:26 -0300

    Move tasks to a javascript file for easier use

---
+++ src/tasks.js
@@ -0,0 +1,14 @@
+export const data = {
+    "Dailies": [
+        {
+            "label": "divine-locations",
+            "name": "Divine Locations",
+            "description": "A divine location is a gathering hub that can be placed almost anywhere using the Divination skill. They are useful for six skills, with divine locations covering a range of levels within those skills.",
+            "link": "http://runescape.wiki/w/Divine_locations",
+            "image": "/RuneTrackLogo.png"
+        }
+    ],
+    "Weeklies": {},
+    "Monthlies": {},
+    "Others": {}
+}
