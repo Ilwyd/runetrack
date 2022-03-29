8d828e243a0e026ed765f7567b7f2c780cc063ea
William1Gray <wgray5093@gmail.com>
2022-03-29 00:29:20 -0300

    Add grid to CardArea

--- src/Components/CardArea/CardArea.tsx
+++ src/Components/CardArea/CardArea.tsx
@@ -1,4 +1,4 @@
-import { Container } from "@mantine/core";
+import { Container, SimpleGrid } from "@mantine/core";
 import { Tabs } from "../../Constants/Tabs";
 import { data } from "../../tasks";
 import { TaskCard } from "../TaskCard/TaskCard";
@@ -16,9 +16,11 @@ function CardArea({ activeTab }: CardAreaProps) {
     else {
         return (
             <Container>
-                {data[activeTab].map((task) => {
-                    return <TaskCard image={task.image} description={task.description} title={task.name} type="Daily" />
-                })}
+                <SimpleGrid cols={3}>
+                    {data[activeTab].map((task) => {
+                        return <TaskCard image={task.image} description={task.description} title={task.name} type="Daily" />
+                    })}
+                </SimpleGrid>
             </Container>
         )
     }
