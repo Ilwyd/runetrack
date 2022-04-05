4e04d662e5e8cb4309a1619583425b724728ce48
William1Gray <wgray5093@gmail.com>
2022-04-05 20:48:00 -0300

    Update task list

--- src/tasks.js
+++ src/tasks.js
@@ -5,28 +5,28 @@ export const data = {
             "name": "Divine Locations",
             "description": "A divine location is a gathering hub that can be placed almost anywhere using the Divination skill. They are useful for six skills, with divine locations covering a range of levels within those skills.",
             "link": "http://runescape.wiki/w/Divine_locations",
-            "image": "/RuneTrackLogo.png"
+            "image": ""
         },
         {
-            "label": "divine-locations",
-            "name": "Divine Locations",
-            "description": "A divine location is a gathering hub that can be placed almost anywhere using the Divination skill. They are useful for six skills, with divine locations covering a range of levels within those skills.",
-            "link": "http://runescape.wiki/w/Divine_locations",
-            "image": "/RuneTrackLogo.png"
+            "label": "soul-obelisks",
+            "name": "Soul Obelisks",
+            "description": "Soul obelisks may appear in Menaphos periodically. They can be interacted with to gain a large amount of Reputation, Runecrafting, and Prayer experience at the cost of receiving some damage over time. If the player does not have level 50 Prayer, no prayer experience will be awarded, and they will instead gain double the Runecrafting experience. ",
+            "link": "https://runescape.wiki/w/Soul_obelisk_(Menaphos)",
+            "image": ""
         },
         {
-            "label": "divine-locations",
-            "name": "Divine Locations",
-            "description": "A divine location is a gathering hub that can be placed almost anywhere using the Divination skill. They are useful for six skills, with divine locations covering a range of levels within those skills.",
-            "link": "http://runescape.wiki/w/Divine_locations",
-            "image": "/RuneTrackLogo.png"
+            "label": "skilling-helms",
+            "name": "Modified Skilling Helms",
+            "description": "Items can be claimed daily from the modified skilling outfit helmets.",
+            "link": "https://runescape.wiki/w/Category:Modified_skill_helms",
+            "image": ""
         },
         {
-            "label": "divine-locations",
-            "name": "Divine Locations",
-            "description": "A divine location is a gathering hub that can be placed almost anywhere using the Divination skill. They are useful for six skills, with divine locations covering a range of levels within those skills.",
-            "link": "http://runescape.wiki/w/Divine_locations",
-            "image": "/RuneTrackLogo.png"
+            "label": "nemi-forest",
+            "name": "Nemi Forest",
+            "description": "A randomly generated forest on Mazcab. Throughout the forest, there are random activities that grant experience as well as 5 Goebie reputation each. A total number of 45 reputation can be earned each day.",
+            "link": "https://runescape.wiki/w/Nemi_Forest",
+            "image": ""
         }
     ],
     "Weeklies": [],
