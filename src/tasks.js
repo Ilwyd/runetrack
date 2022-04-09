3419f645c8929f243684c9b4c5e67e8e6eadc992
William1Gray <wgray5093@gmail.com>
2022-04-08 21:01:11 -0300

    Add daily tasks with just name and label

--- src/tasks.js
+++ src/tasks.js
@@ -7,8 +7,8 @@ export const data = {
             "link": "http://runescape.wiki/w/Divine_locations",
             "image": "",
             "rewards": {
-                "xp": ["Various non-combat skills"],
-                "loot": ["Various skilling materials"]
+                "xp": ["Fishing", "Woodcutting", "Farming", "Mining", "Hunter", "Divination"],
+                "loot": ["Fish", "Logs", "Herbs", "Ores", "General Hunter loot", "Signs", "Portents", "Energies"]
             }
         },
         {
@@ -41,6 +41,490 @@ export const data = {
             "rewards": {
                 "xp": ["Dungeoneering", "Farming", "Mining", "Prayer"],
                 "reputation": ["Goebie"]
+            }
+        },
+        {
+            "label": "wilderness-warbands",
+            "name": "Wilderness Warbands",
+            "description": "",
+            "link": "",
+            "image": "",
+            "rewards": {
+                "xp": [],
+                "reputation": []
+            }
+        },
+        {
+            "label": "supply-run",
+            "name": "Supply Run",
+            "description": "",
+            "link": "",
+            "image": "",
+            "rewards": {
+                "xp": [],
+                "reputation": []
+            }
+        },
+        {
+            "label": "shooting-star",
+            "name": "Shooting Star",
+            "description": "",
+            "link": "",
+            "image": "",
+            "rewards": {
+                "xp": [],
+                "reputation": []
+            }
+        },
+        {
+            "label": "sinkhole",
+            "name": "Sinkhole",
+            "description": "",
+            "link": "",
+            "image": "",
+            "rewards": {
+                "xp": [],
+                "reputation": []
+            }
+        },
+        {
+            "label": "evil-trees",
+            "name": "Evil Trees",
+            "description": "",
+            "link": "",
+            "image": "",
+            "rewards": {
+                "xp": [],
+                "reputation": []
+            }
+        },
+        {
+            "label": "big-chinchompa",
+            "name": "Big Chinchompa",
+            "description": "",
+            "link": "",
+            "image": "",
+            "rewards": {
+                "xp": [],
+                "reputation": []
+            }
+        },
+        {
+            "label": "guthixian-cache",
+            "name": "Guthixian Cache",
+            "description": "",
+            "link": "",
+            "image": "",
+            "rewards": {
+                "xp": [],
+                "reputation": []
+            }
+        },
+        {
+            "label": "runesphere",
+            "name": "Runesphere",
+            "description": "",
+            "link": "",
+            "image": "",
+            "rewards": {
+                "xp": [],
+                "reputation": []
+            }
+        },
+        {
+            "label": "serenity-posts",
+            "name": "Serenity Posts",
+            "description": "",
+            "link": "",
+            "image": "",
+            "rewards": {
+                "xp": [],
+                "reputation": []
+            }
+        },
+        {
+            "label": "crystal-tree-blossom",
+            "name": "Crystal Tree Blossom",
+            "description": "",
+            "link": "",
+            "image": "",
+            "rewards": {
+                "xp": [],
+                "reputation": []
+            }
+        },
+        {
+            "label": "jack-of-trades",
+            "name": "Jack of Trades",
+            "description": "",
+            "link": "",
+            "image": "",
+            "rewards": {
+                "xp": [],
+                "reputation": []
+            }
+        },
+        {
+            "label": "death-task",
+            "name": "Death Task",
+            "description": "",
+            "link": "",
+            "image": "",
+            "rewards": {
+                "xp": [],
+                "reputation": []
+            }
+        },
+        {
+            "label": "bork",
+            "name": "Bork",
+            "description": "",
+            "link": "",
+            "image": "",
+            "rewards": {
+                "xp": [],
+                "reputation": []
+            }
+        },
+        {
+            "label": "phoenix",
+            "name": "Phoenix",
+            "description": "",
+            "link": "",
+            "image": "",
+            "rewards": {
+                "xp": [],
+                "reputation": []
+            }
+        },
+        {
+            "label": "red-sandstone",
+            "name": "Red Sandstone",
+            "description": "",
+            "link": "",
+            "image": "",
+            "rewards": {
+                "xp": [],
+                "reputation": []
+            }
+        },
+        {
+            "label": "crystal-flecked-sandstone",
+            "name": "Crystal Flecked Sandstone",
+            "description": "",
+            "link": "",
+            "image": "",
+            "rewards": {
+                "xp": [],
+                "reputation": []
+            }
+        },
+        {
+            "label": "gorajo-card",
+            "name": "Gorajo Card",
+            "description": "",
+            "link": "",
+            "image": "",
+            "rewards": {
+                "xp": [],
+                "reputation": []
+            }
+        },
+        {
+            "label": "book-of-char",
+            "name": "Book of Char",
+            "description": "",
+            "link": "",
+            "image": "",
+            "rewards": {
+                "xp": [],
+                "reputation": []
+            }
+        },
+        {
+            "label": "yak-hide",
+            "name": "Yak Hide",
+            "description": "",
+            "link": "",
+            "image": "",
+            "rewards": {
+                "xp": [],
+                "reputation": []
+            }
+        },
+        {
+            "label": "fresh-meat-shop",
+            "name": "Fresh Meat Shop",
+            "description": "",
+            "link": "",
+            "image": "",
+            "rewards": {
+                "xp": [],
+                "reputation": []
+            }
+        },
+        {
+            "label": "arheins-shop",
+            "name": "Arhein's Shop",
+            "description": "",
+            "link": "",
+            "image": "",
+            "rewards": {
+                "xp": [],
+                "reputation": []
+            }
+        },
+        {
+            "label": "potato-cacti",
+            "name": "Collect Potato Cacti from the Weird Old Man",
+            "description": "",
+            "link": "",
+            "image": "",
+            "rewards": {
+                "xp": [],
+                "reputation": []
+            }
+        },
+        {
+            "label": "pineapples",
+            "name": "Collect noted pineapples from Dell Monti",
+            "description": "",
+            "link": "",
+            "image": "",
+            "rewards": {
+                "xp": [],
+                "reputation": []
+            }
+        },
+        {
+            "label": "flax",
+            "name": "Collect Flax from Geoffrey",
+            "description": "",
+            "link": "",
+            "image": "",
+            "rewards": {
+                "xp": [],
+                "reputation": []
+            }
+        },
+        {
+            "label": "buckets-of-sand",
+            "name": "Collect Buckets of Sand from Bert",
+            "description": "",
+            "link": "",
+            "image": "",
+            "rewards": {
+                "xp": [],
+                "reputation": []
+            }
+        },
+        {
+            "label": "pure-essence",
+            "name": "Collect Pure Essence from Wizard Cromperty",
+            "description": "",
+            "link": "",
+            "image": "",
+            "rewards": {
+                "xp": [],
+                "reputation": []
+            }
+        },
+        {
+            "label": "bag-of-lost-items",
+            "name": "Collect Bag of Lost Items from a Rug Merchant",
+            "description": "",
+            "link": "",
+            "image": "",
+            "rewards": {
+                "xp": [],
+                "reputation": []
+            }
+        },
+        {
+            "label": "explorers-ring",
+            "name": "Use the Explorer's Ring to cast free spells",
+            "description": "",
+            "link": "",
+            "image": "",
+            "rewards": {
+                "xp": [],
+                "reputation": []
+            }
+        },
+        {
+            "label": "molten-glass",
+            "name": "Use the Desert Amulet to Make Molten Glass",
+            "description": "",
+            "link": "",
+            "image": "",
+            "rewards": {
+                "xp": [],
+                "reputation": []
+            }
+        },
+        {
+            "label": "rune-goldberg-machine",
+            "name": "Rune Goldberg Machine",
+            "description": "",
+            "link": "",
+            "image": "",
+            "rewards": {
+                "xp": [],
+                "reputation": []
+            }
+        },
+        {
+            "label": "wicked-hood",
+            "name": "Wicked Hood",
+            "description": "",
+            "link": "",
+            "image": "",
+            "rewards": {
+                "xp": [],
+                "reputation": []
+            }
+        },
+        {
+            "label": "planks",
+            "name": "Collect Planks from Razmire",
+            "description": "",
+            "link": "",
+            "image": "",
+            "rewards": {
+                "xp": [],
+                "reputation": []
+            }
+        },
+        {
+            "label": "food-hamper",
+            "name": "Collect the Food Hamper from the Cook in the Lumbridge Castle Basement",
+            "description": "",
+            "link": "",
+            "image": "",
+            "rewards": {
+                "xp": [],
+                "reputation": []
+            }
+        },
+        {
+            "label": "motherlode-maw",
+            "name": "Collect an item from the Motherlode Maw",
+            "description": "",
+            "link": "",
+            "image": "",
+            "rewards": {
+                "xp": [],
+                "reputation": []
+            }
+        },
+        {
+            "label": "motherlode-shards",
+            "name": "Exchange Crystal Motherlode Shards with Wythien",
+            "description": "",
+            "link": "",
+            "image": "",
+            "rewards": {
+                "xp": [],
+                "reputation": []
+            }
+        },
+        {
+            "label": "broad-arrowheads",
+            "name": "Buy Broad Arrowheads / Unfinished Broad Bolts from a Slayer Master",
+            "description": "",
+            "link": "",
+            "image": "",
+            "rewards": {
+                "xp": [],
+                "reputation": []
+            }
+        },
+        {
+            "label": "rune-run",
+            "name": "Buy Runes from various shops around Runescape",
+            "description": "",
+            "link": "",
+            "image": "",
+            "rewards": {
+                "xp": [],
+                "reputation": []
+            }
+        },
+        {
+            "label": "logs",
+            "name": "Collect 28 Logs from Coeden",
+            "description": "",
+            "link": "",
+            "image": "",
+            "rewards": {
+                "xp": [],
+                "reputation": []
+            }
+        },
+        {
+            "label": "trinks-tasks",
+            "name": "Trinks Tasks",
+            "description": "",
+            "link": "",
+            "image": "",
+            "rewards": {
+                "xp": [],
+                "reputation": []
+            }
+        },
+        {
+            "label": "rosie",
+            "name": "Collect free supplies from Rosie",
+            "description": "",
+            "link": "",
+            "image": "",
+            "rewards": {
+                "xp": [],
+                "reputation": []
+            }
+        },
+        {
+            "label": "arc-contracts",
+            "name": "Complete Arc Contracts",
+            "description": "",
+            "link": "",
+            "image": "",
+            "rewards": {
+                "xp": [],
+                "reputation": []
+            }
+        },
+        {
+            "label": "claimed-island",
+            "name": "Collect resources from your Claimed Island",
+            "description": "",
+            "link": "",
+            "image": "",
+            "rewards": {
+                "xp": [],
+                "reputation": []
+            }
+        },
+        {
+            "label": "bounties",
+            "name": "Complete Heart of Gielinor Bounties",
+            "description": "",
+            "link": "",
+            "image": "",
+            "rewards": {
+                "xp": [],
+                "reputation": []
+            }
+        },
+        {
+            "label": "treasure-hunter-keys",
+            "name": "Use your daily Treasure Hunter Keys",
+            "description": "",
+            "link": "",
+            "image": "",
+            "rewards": {
+                "xp": [],
+                "reputation": []
             }
         }
     ],
